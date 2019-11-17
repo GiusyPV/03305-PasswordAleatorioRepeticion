2996b15c2f746c77fe3584f95501bc096b9b8003
Giusy <Giusy@192.168.1.129>
2019-11-17 19:17:46 +0100

    :tada: Primer Commit

---
+++ src/org/japo/java/main/Main.java
@@ -0,0 +1,46 @@
+/* 
+ * Copyright 2019 Josefina Pugliese Vazquez - josefinapwork@gmail.com.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.japo.java.main;
+
+import java.util.Random;
+
+/**
+ *
+ * @author Josefina Pugliese Vazquez
+ */
+public class Main {
+
+    public static final Random RND = new Random();
+
+    public static void main(String[] args) {
+
+        //Constantes
+        final char LETRA_MIN = 'A';
+        final char LETRA_MAX = 'Z';
+        final int NUM_MAX = 8;
+
+        //Bucle
+        for (int i = 0; i < NUM_MAX; i++) {
+            char c = (char) (RND.nextInt(LETRA_MAX - LETRA_MIN) + LETRA_MIN);
+
+            // Mensaje Consola
+            System.out.print(c);
+
+        }
+        System.out.println("");
+
+    }
+}
